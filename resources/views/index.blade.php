48a9132b84c8088c43f5127ca6554edad8f25d23
w1lldone <wildan.arrahman@gmail.com>
2018-07-02 09:03:11 +0700

    Update index.blade.php for SPA

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,11 +1,5 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col">
-            <h1>Welcome to Genebank</h1>
-        </div>
-    </div>
-</div>
+    <router-view></router-view>
 @endsection
