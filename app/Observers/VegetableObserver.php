b4b1dbca0dc9856383ec3a3a0255e9e8be898b04
w1lldone <wildan.arrahman@gmail.com>
2018-07-24 23:29:21 +0700

    Attach evaluation id after vegetable has been created

--- app/Observers/VegetableObserver.php
+++ app/Observers/VegetableObserver.php
@@ -3,6 +3,7 @@
 namespace App\Observers;
 
 use App\Attribute;
+use App\Category;
 use App\Genus;
 use App\Vegetable;
 
@@ -20,10 +21,21 @@ class VegetableObserver
 
         // Get array of characters name based related to genus name
         $fields = Genus::charactersList($vegetable->species->genus->name);
+
         // Get array of characters id
         $characters = Attribute::whereIn('name', $fields)->get()->pluck('id');
-        // attach the character id to vegetable
-        $vegetable->attributes()->attach($characters);
+
+        // Get evaluation category ids
+        $categories = Category::getEvaluationCharacters();
+
+        // Get evaluation attribute id
+        $evaluations = Attribute::whereIn('category_id', $categories)->pluck('id');
+
+        // Merge evaluation id with character id
+        $attributes = $evaluations->merge($characters);
+
+        // attach the attribute id to vegetable
+        $vegetable->attributes()->attach($attributes);
 
         // ADD PASSPORT TO VEGETABLE
         $vegetable->passport()->create([]);
