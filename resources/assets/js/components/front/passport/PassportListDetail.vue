8165a73132952ef2f0c7bd436683945ca6f30b7c
w1lldone <wildan.arrahman@gmail.com>
2018-07-31 14:00:20 +0700

    Fix passport details components

--- resources/assets/js/components/front/passport/PassportListDetail.vue
+++ resources/assets/js/components/front/passport/PassportListDetail.vue
@@ -1,35 +1,44 @@
 <template>
-   <div class="detail">
-        <div id="button">
-            <button class="col-sm-12" align:"left" onclick="Passport()">nutritive value</button>
-            <form action="" class="form-horizontal" role="form" name="passportTable" id="passportTable">
-                <div class="form-group form-inline" align="right">
-                    <div class="col-sm-12" >
-                        <label>Vegetable introduction number &emsp;</label>
-                        <input type="text" class="form-control" name="number" id="number" placeholder="VI000306"><br>
-                        <label>Crop accession number &emsp;</label>
-                        <input type="text" class="form-control" name="crop_number" id="crop_number" placeholder="V01306"><br>
-                        <label>Family &emsp;</label>
-                        <input type="text" class="form-control" name="family" id="family" placeholder="LEGUMINOSAE"><br>
-                        <label>Genus &emsp;</label>
-                        <input type="text" class="form-control" name="genus" id="genus" placeholder="VIGNA"><br>
-                        <label>Species &emsp;</label>
-                        <input type="text" class="form-control" name="species" id="species" placeholder="MUNGO"><br>
+    <front-base>
+        <section>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h4><b>Passport Data VI000002</b></h4>
+                        <br>
+                        <p>Refers to accession no. and data recorded when the sample was originally collected (i.e. crop accession no., genus, species, altitude, collecting location, etc.). These data were received from the seed donor together with the germplasm. </p><br><br>
                     </div>
                 </div>
-            </form>
-        </div>
-    </div>
+                <div class="row">
+                    <div class="col-md-6 table-responsive">
+                        <table class="table">
+                            <tbody>
+                                <tr>
+                                    <td>Vegetable introduction number</td>
+                                    <td>VI000002</td>
+                                </tr>
+                                <tr>
+                                    <td>Family</td>
+                                    <td>LEGUMINOSAE</td>
+                                </tr>
+                                <tr>
+                                    <td>Species</td>
+                                    <td>LEGUMINOSAE</td>
+                                </tr>
+                                <!-- PLEASE CONTINUE THE TABLE BASED ON PASSPORT DETAILS ON SEED.WORLDVEG.ORG -->
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </front-base>
+   
 </template>
 
 <script>
-function Passport() {
-    var x = document.getElementById("passportTable");
-        if (x.style.display === "none") {
-            x.style.display = "block";
-        } else {
-            x.style.display = "none";
-        }}
+import FrontBase from '../FrontBase';
+
 export default {
 
   name: 'PassportListDetail',
@@ -37,6 +46,9 @@ export default {
     return {
         load: 'passport',
     }
+  },
+  components: {
+    FrontBase,
   }
 }
 </script>
