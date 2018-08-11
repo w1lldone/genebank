0a031e6659bde920ef521f52ff023230dc51c42e
w1lldone <wildan.arrahman@gmail.com>
2018-08-12 00:14:34 +0700

    Add breadcrumb to vegetable page

--- resources/views/admin/vegetable/index.blade.php
+++ resources/views/admin/vegetable/index.blade.php
@@ -1,7 +1,13 @@
 @extends('admin.layouts.basic')
 
 @section('breadcrumb')
-    
+    <!-- Breadcrumbs-->
+    <ol class="breadcrumb">
+      <li class="breadcrumb-item">
+        <a href="#">Vegetable</a>
+      </li>
+      <li class="breadcrumb-item active">Overview</li>
+    </ol>
 @endsection
 
 @section('content')
