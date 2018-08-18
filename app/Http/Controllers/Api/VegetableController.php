0e7d8b768cad0293c30020a2513c71e43a30416c
w1lldone <wildan.arrahman@gmail.com>
2018-08-18 11:56:01 +0700

    Fix vegetables filtering

--- app/Http/Controllers/Api/VegetableController.php
+++ app/Http/Controllers/Api/VegetableController.php
@@ -12,48 +12,48 @@ class VegetableController extends Controller
 {
     public function filter($model, $request)
     {
-        if ($request->has('load')) {
+        if ($request->filled('load')) {
             $model = $model->with($request->load);
         }
 
-        if ($request->has('genus_id')) {
+        if ($request->filled('genus_id')) {
             $model = $model->whereHas('species', function ($query) use ($request)
             {
                 $query->where('genus_id', $request->genus_id);
             });
         }
 
-        if ($request->has('species_id')) {
+        if ($request->filled('species_id')) {
             $model = $model->where('species_id', "$request->species_id");
         }
 
-        if ($request->has('plant_introduction_number')) {
+        if ($request->filled('plant_introduction_number')) {
             $model = $model->where('plant_introduction_number', 'like', "%$request->plant_introduction_number%");
         }
 
-        if ($request->has('temporary_number')) {
+        if ($request->filled('temporary_number')) {
             $model = $model->where('temporary_number', 'like', "%$request->temporary_number%");
         }
 
-        if ($request->has('donor_number')) {
+        if ($request->filled('donor_number')) {
             $model = $model->whereHas('passport', function ($query) use ($request)
             {
                 $query->where('donor_number', 'like', "%$request->donor_number%");
             });
         }
 
-        if ($request->has('cultivar_name')) {
+        if ($request->filled('cultivar_name')) {
             $model = $model->where('cultivar_name', 'like', "%$request->cultivar_name%");
         }
 
-        if ($request->has('country')) {
+        if ($request->filled('country')) {
             $model = $model->whereHas('passport', function ($query) use ($request)
             {
                 $query->where('country', 'like', "%$request->country%");
             });
         }
 
-        if ($request->has('distrbution_status')) {
+        if ($request->filled('distrbution_status')) {
             $model = $model->whereHas('passport', function ($query) use ($request)
             {
                 $query->where('distrbution_status', 'like', "%$request->distrbution_status%");
