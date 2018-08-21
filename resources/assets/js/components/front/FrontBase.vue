8b25b6017990a52e3686492e353b7c548d88e3f9
w1lldone <wildan.arrahman@gmail.com>
2018-08-21 22:48:49 +0700

    Fix Footer

--- resources/assets/js/components/front/FrontBase.vue
+++ resources/assets/js/components/front/FrontBase.vue
@@ -3,30 +3,45 @@
         <navbar></navbar>
         <slot></slot>
         <!--Start of footer-->
-        <section id="footer">
+        <footer>
             <div class="container">
-                <div class="row ">
-                    <!-- <div class="col-md-6">
-                        <div class="copyright">
-                            <p >@ 2018 - Copyright <span><a href=""> East-West Seed Indonesia</a></span></p>
+                <div class="row">
+                    <div class="col-md-4">
+                        <h5>Bank Sumberdaya Genetik Sayuran</h5>
+                        <span>Supported by:</span>
+                        <div class="">
+                            <a href="http://www.panahmerah.id/"><img src="/img/PanahMerah.png" width="100"></a>
+                            <br>
+                            <a href="http://www.panahmerah.id/">PT East West Seed Indonesia</a>
                         </div>
-                    </div> -->
-                    <div class="support" style="align-content: right;">
-                       <p style="text-align: center;">@ 2018 - Copyright <span><a href=""> East-West Seed Indonesia</a></span></p>
-                        &emsp;&emsp;&emsp;<p>Supported By : &emsp;</p>
-                        <img src="/img/PanahMerah.png">
                     </div>
-                   <!--  <div class="col-md-6">
-                        <div class="copyright" style="text-align:center">
-                            <p >@ 2018 - Copyright <span><a href=""> East-West Seed Indonesia</a></span></p>
-                        </div>
-                    </div> -->
-                    
+                    <div class="col-md-5">
+                        <h5>Kontak</h5>
+                        <span>
+                            <a href="http://piat.ugm.ac.id" target="_blank">Pusat Inovasi Agroteknologi</a> <br>
+                            Universitas Gadjah Mada <br>
+                            Kalitirto, Berbah, Sleman, Yogyakarta 55573, Indonesia <br>
+                            <i class="fa fa-envelope fa-fw"></i> <a href="mailto:piat@ugm.ac.id">piat@ugm.ac.id</a> <br>
+                            <i class="fa fa-phone fa-fw"></i> +62 (274) 497717 <br>
+                            <i class="fa fa-fax fa-fw"></i> +62 (274) 497717 <br>
+                        </span>
+                    </div>
+                    <div class="col-md-3">
+                        <h5>Navigasi</h5>
+                        <a href="/about">Tentang Kami</a> <br>
+                        <a href="/terms">Syarat dan Ketentuan</a> <br>
+                        <a href="/download">Unduh</a> <br>
+                        <a href="/gallery">Galeri</a>
+                    </div>
                 </div>
-                <!--End of row-->
+                <hr>
+                <div class="row">
+                    <div class="col-lg-12 text-center">
+                        &copy; 2018 <a href="http://www.panahmerah.id/">PT East West Seed Indonesia</a>. Developed by <a href="http://smartcity.wg.ugm.ac.id/" target="_blank">Smart System Research Group</a>.
+                    </div>
+                </div>
             </div>
-            <!--End of container-->
-        </section>
+        </footer>
         <!--End of footer-->
     </div>
 </template>
@@ -50,4 +65,18 @@ export default {
 </script>
 
 <style lang="css" scoped>
+ footer {
+    background: #cacfd2;
+    padding-top: 5rem;
+    padding-bottom: 3rem;
+ }
+
+ hr {
+    border-top-color: #a2a2a2;
+ }
+
+ h5 {
+    margin-top: 2rem;
+    margin-bottom: 1rem;
+ }
 </style>
